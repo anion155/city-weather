efad3f628ce7643d8e1abc37954518e7a29230f3
Mikhaylov Anton <anion155@gmail.com>
2020-05-09 21:46:56 +0500

    feat: weather types

--- src/OpenWeatherService.ts
+++ src/OpenWeatherService.ts
@@ -3,10 +3,16 @@ import React from 'react';
 export enum Units {
   kelvin, metric, imperial
 }
+export const UnitsTemp = {
+  [Units.kelvin]: '',
+  [Units.metric]: '°',
+  [Units.imperial]: '',
+};
 
 export const OpenWeatherService = new class OpenWeatherService {
   serviceOrigin = 'http://api.openweathermap.org';
   apiPath = '/data/2.5';
+  iconsPath = '/img/w';
   appid = build.OPENWEATHER_APPID;
 
   async forecast(cityQuery: CityQuery, units?: Units, lang?: string, maxLinesCount?: number): Promise<OpenWeatherForecast> {
@@ -39,40 +45,56 @@ export const OpenWeatherService = new class OpenWeatherService {
     if (!resp.ok) throw new Error((await resp.json()).message);
     return await resp.json();
   }
+
+  icon(id: string) {
+    const url = new URL(`${this.iconsPath}/${id}.png`, this.serviceOrigin);
+    return url.href;
+  }
 }
 
-export function useOpenWeatherForecast(cityQuery?: CityQuery, units?: Units, lang?: string, maxLinesCount?: number) {
+interface OpenWeatherForecastOptions {
+  units?: Units;
+  lang?: string;
+  maxLinesCount?: number;
+}
+export interface OpenWeatherResult {
+  date: number;
+  units: Units;
+  forecast: OpenWeatherForecast;
+}
+export function useOpenWeatherForecast(query?: CityQuery, { units = Units.metric, lang, maxLinesCount }: OpenWeatherForecastOptions = { }) {
   const promiseRef = React.useRef<Promise<any>>();
   const [ loading, setLoading ] = React.useState<boolean>(false);
-  const [ forecast, setForecast ] = React.useState<OpenWeatherForecast>();
+  const [ result, setResult ] = React.useState<OpenWeatherResult>();
   const [ error, setError ] = React.useState<any>();
 
   React.useEffect(() => {
-    if (!cityQuery) return;
+    if (!query) return;
 
     setLoading(true);
     setError(undefined);
-    const promise = OpenWeatherService.forecast(cityQuery, units, lang, maxLinesCount);
+    const date = Date.now();
+    const promise = OpenWeatherService.forecast(query, units, lang, maxLinesCount);
     promiseRef.current = promise;
     promise.then(forecast => {
       console.log(forecast);
       if (promise !== promiseRef.current) return;
       setLoading(false);
-      setForecast(forecast);
+      setResult({ date, units, forecast });
     }).catch(error => {
       console.error(error);
       if (promise !== promiseRef.current) return;
       setLoading(false);
-      setForecast(undefined);
+      setResult(undefined);
       setError(error);
     });
     return () => {
       promiseRef.current = undefined;
       setLoading(false);
     };
-  }, [ cityQuery, units, lang, maxLinesCount ]);
+  }, [ query, units, lang, maxLinesCount ]);
 
-  return React.useMemo(() => ({ loading, forecast, error }), [ loading, forecast, error ]);
+  return React.useMemo(() => ({ loading, result, error }), [ loading, result, error ]);
 }
 
 export interface Coords {
@@ -86,51 +108,81 @@ export type CityQuery = (
   | { type: 'zip'; zipcode: number; countryCode: string }
 );
 export interface OpenWeatherForecast {
-  code: number; // Internal parameter
-  message: string; // Internal parameter
+  /** Internal parameter */
+  code: number;
+  /** Internal parameter */
+  message: string;
   city: {
-    id: string; // City ID
-    name: string; // City name
-    coord: Coords; // City geo location
-    country: string; // Country code (GB, JP etc.)
-    timezone: number; // Shift in seconds from UTC
+    /** City ID */
+    id: string;
+    /** City name */
+    name: string;
+    /** City geo location */
+    coord: Coords;
+    /** Country code (GB, JP etc.) */
+    country: string;
+    /** Shift in seconds from UTC */
+    timezone: number;
     population: number;
     sunrise: number;
     sunset: number;
   };
-  cnt: number; // Number of lines returned by this API call
-  list: {
-    dt: number; // Time of data forecasted, unix, UTC
-    main: {
-      temp: number; // Temperature. Unit Default: Kelvin, Metric: Celsius, Imperial: Fahrenheit
-      feels_like: string; // Temperature. This temperature parameter accounts for the human perception of weather. Unit Default: Kelvin, Metric: Celsius, Imperial: Fahrenheit
-      temp_min: string | void; // Minimum temperature at the moment of calculation. This is minimal forecasted temperature (within large megalopolises and urban areas), use this parameter optionally. Unit Default: Kelvin, Metric: Celsius, Imperial: Fahrenheit
-      temp_max: string | void; // Maximum temperature at the moment of calculation. This is maximal forecasted temperature (within large megalopolises and urban areas), use this parameter optionally. Unit Default: Kelvin, Metric: Celsius, Imperial: Fahrenheit
-      pressure: number; // Atmospheric pressure on the sea level by default, hPa
-      sea_level: number; // Atmospheric pressure on the sea level, hPa
-      grnd_level: number; // Atmospheric pressure on the ground level, hPa
-      humidity: number; // Humidity, %
-      temp_kf: number; // Internal parameter
-    };
-    weather: {
-      id: any; // Weather condition id
-      main: string; // Group of weather parameters (Rain, Snow, Extreme etc.)
-      description: string; // Weather condition within the group
-      icon: string; // Weather icon id
-    }; // (more info Weather condition codes)
-    clouds: {
-      all: number; // Cloudiness, %
-    };
-    wind: {
-      speed: number; // Wind speed. Unit Default: meter/sec, Metric: meter/sec, Imperial: miles/hour.
-      deg: number; // Wind direction, degrees (meteorological)
-    };
-    rain: {
-      '3h': number; // Rain volume for last 3 hours, mm
-    };
-    snow: {
-      '3h': number; // Snow volume for last 3 hours, mm
-    };
-    dt_txt: number; // Time of data forecasted, ISO, UTC
+  /** Number of lines returned by this API call */
+  cnt: number;
+  list: OpenWeatherForecastItem[];
+}
+export interface OpenWeatherForecastItem {
+  /** Time of data forecasted, unix, UTC */
+  dt: number;
+  main: {
+    /** Temperature. Unit Default: Kelvin, Metric: Celsius, Imperial: Fahrenheit */
+    temp: number;
+    /** Temperature. This temperature parameter accounts for the human perception of weather. Unit Default: Kelvin, Metric: Celsius, Imperial: Fahrenheit */
+    feels_like: number;
+    /** Minimum temperature at the moment of calculation. This is minimal forecasted temperature (within large megalopolises and urban areas), use this parameter optionally. Unit Default: Kelvin, Metric: Celsius, Imperial: Fahrenheit */
+    temp_min: string | void;
+    /** Maximum temperature at the moment of calculation. This is maximal forecasted temperature (within large megalopolises and urban areas), use this parameter optionally. Unit Default: Kelvin, Metric: Celsius, Imperial: Fahrenheit */
+    temp_max: string | void;
+    /** Atmospheric pressure on the sea level by default, hPa */
+    pressure: number;
+    /** Atmospheric pressure on the sea level, hPa */
+    sea_level: number;
+    /** Atmospheric pressure on the ground level, hPa */
+    grnd_level: number;
+    /** Humidity, % */
+    humidity: number;
+    /** Internal parameter */
+    temp_kf: number;
+  };
+  /** (more info Weather condition codes) */
+  weather: {
+    /** Weather condition id */
+    id: any;
+    /** Group of weather parameters (Rain, Snow, Extreme etc.) */
+    main: string;
+    /** Weather condition within the group */
+    description: string;
+    /** Weather icon id */
+    icon: string;
   }[];
+  clouds: {
+    /** Cloudiness, % */
+    all: number;
+  };
+  wind: {
+    /** Wind speed. Unit Default: meter/sec, Metric: meter/sec, Imperial: miles/hour. */
+    speed: number;
+    /** Wind direction, degrees (meteorological) */
+    deg: number;
+  };
+  rain: {
+    /** Rain volume for last 3 hours, mm */
+    '3h': number;
+  };
+  snow: {
+    /** Snow volume for last 3 hours, mm */
+    '3h': number;
+  };
+  /** Time of data forecasted, ISO, UTC */
+  dt_txt: number;
 }
